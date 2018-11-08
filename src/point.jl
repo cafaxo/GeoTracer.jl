57aa227b40b0cb6950aa2b4ab18eaadc0d2c2dd4
cafaxo <cafaxo@gmail.com>
2018-11-08 01:47:11 +0100

    Update point.jl

--- src/point.jl
+++ src/point.jl
@@ -120,10 +120,10 @@ function intersect_line_circle(l::Point, c::CircleMatrix)
 
     erg = s + alp * l1
 
-    erg1 = erg[argmax((norm(erg[1,:]), norm(erg[2,:]), norm(erg[3,:]))),:]
-    erg2 = erg[:,argmax((norm(erg[:,1]), norm(erg[:,2]), norm(erg[:,3])))]
+    erg1 = Point(erg[argmax((norm(erg[1,:]), norm(erg[2,:]), norm(erg[3,:]))),:])
+    erg2 = Point(erg[:,argmax((norm(erg[:,1]), norm(erg[:,2]), norm(erg[:,3])))])
 
-    return Point(erg1), Point(erg2)
+    return erg1 / abs(erg1), erg2 / abs(erg2)
 end
 
 function intersect_circle_circle(c1::CircleMatrix, c2::CircleMatrix)
